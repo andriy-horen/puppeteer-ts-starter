cdc8f3d8887e1dc5b76c3c6733673e6177da6cb1
andriy-horen <go.andriy@gmail.com>
2020-03-08 17:27:07 +0100

    update dependency versions ⬆️

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import puppeteer from 'puppeteer';
 
 async function bootstrap() {
     const browser = await puppeteer.launch({
-        headless: false
+        headless: false,
     });
 
     const page = (await browser.pages())[0];
-    await page.goto('https://github.com/GoogleChrome/puppeteer/');
+    await page.goto('https://github.com/puppeteer/puppeteer#puppeteer');
 }
 
 bootstrap();
